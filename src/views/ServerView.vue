438a06c3456eab0c658ef4773ddb1109f8f7d8b1
abimanyudrmwn <abimanyudarmawan4@gmail.com>
2023-01-19 06:11:46 +0700

    fixing darkmode

--- src/views/ServerView.vue
+++ src/views/ServerView.vue
@@ -75,8 +75,8 @@
       </div>
 
   <div class="relative overflow-x-auto">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <table class="w-full text-sm text-left text-gray-400">
+        <thead class="text-xs 0 uppercase bg-gray-700 text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
                     Player Name
@@ -90,8 +90,8 @@
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap dark:text-white">
+            <tr class=" border-b bg-gray-800 border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap text-white">
                     Lorem Ipsum
                 </th>
                 <td class="px-6 py-4">
