ef31847b146bc1153f83fda691fb59d146646fe4
abimanyudrmwn <abimanyudarmawan4@gmail.com>
2023-01-19 10:20:04 +0700

    Configure Dummy Apis

--- src/views/vehicles/VehicleDetails.vue
+++ src/views/vehicles/VehicleDetails.vue
@@ -1,14 +1,25 @@
 <template>
-    <main class="bg-gray-900 h-full w-full py-32 font-poppins">
-        <div v-if="vehicle" class="text-white">
-        <h1>{{ vehicle.title }}</h1>
-        <p>The vehicle id is {{ vehicle.id }}</p>
-        <p>{{ vehicle.details }}</p>
-        </div>
-        <div v-else>
-            <p>Loading vehicle details.....</p>
-        </div>
-    </main>
+    <div class="bg-slate-900 font-poppins">
+        <article v-if="vehicle" class="mx-[50vh] max-w-5xl tracking-wide px-6 xl:px-0 prose lg:prose-xl py-20">
+            <p class="text-sm font-md text-white py-16 text-center"><router-link to="/vehicles" class="text-white text-center">← Back to Catalogue</router-link></p>
+                <div class="container py-2 mx-auto lg:flex lg:h-128">
+                    <div class="flex flex-col items-center w-full lg:flex-row lg:w-1/2">
+                        <div class="max-w-lg">
+                            <h1 class="text-xl font-bold tracking-wide text-white lg:text-3xl">{{ vehicle.title }}</h1>
+                            <div class="mt-6">
+                                <div
+                                    class="inline-block px-4 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-full hover:bg-blue-400">
+                                    {{ vehicle.price }} Rupiah</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex items-center justify-center w-full mt-2 lg:h-96 lg:w-1/2">
+                        <img class="object-cover w-full max-w-2xl rounded-md lg:h-full"
+                        :src="vehicle.img" :alt="vehicle.title">
+                    </div>
+            </div>
+        </article>
+    </div>
 </template>
 
 <script>
@@ -20,7 +31,7 @@ export default {
         }
     },
     mounted(){
-        fetch('http://localhost:3000/vehicles/' + this.$route.params.id)
+        fetch('https://ipz-jsonserver-dev.vercel.app/vehicles/' + this.$route.params.id)
             .then(res => res.json())
             .then(data => this.vehicle = data)
             .catch(err => console.log(err.message))
--- src/views/vehicles/Vehicles.vue
+++ src/views/vehicles/Vehicles.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="bg-gray-900 py-10">
+    <div class="bg-gray-900 py-32 font-poppins">
     <div class="mx-auto max-w-5xl tracking-wide px-4 xl:px-0">
-        <h1 class="font-sora text-2xl text-white">Vehicles Catalogue</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-10 py-10">
+    <h1 class="text-2xl text-white font-bold">Vehicles Catalogue</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 py-10">
       <div v-for="vehicle in vehicles" :key="vehicle.id" class="border border-gray-700 rounded-xl shadow-lg">
             <div class="overflow-hidden rounded-xl" style="background-color:#f9f1f9">
               <a :href="`/vehicles/${vehicle.id}`">
-                <img alt="vehicles Image" class="lazyLoad isLoaded object-fill h-96 w-120" src="https://via.placeholder.com/1100x500">
+                <img alt="vehicles Image" class="lazyLoad isLoaded object-cover h-96 w-120" :src="vehicle.img">
               </a>
             </div>
             <div class="p-3 pr-2"><div class="h-2"></div> 
-            <h2 class="font-sora text-2xl font-bold text-white">
+            <h2 class="text-2xl font-bold text-white">
               <a :href="`/vehicles/${vehicle.id}`">
                 {{ vehicle.title }}
               </a>
             </h2> 
                 <div class="h-1"></div>
-                <p class="font-sora text-md font-light text-gray-200 leading-5">Specifications :</p>
+                <p class="text-md font-light text-gray-200 leading-5">Specifications :</p>
                 <ul class="flex justify-between text-white pr-2">
                     <li class="underline">
                         Type
                     </li>
                     <li class="font-bold">
-                        Lorem ipsum
+                        {{ vehicle.type }}
                     </li>
                 </ul>
                 <ul class="flex justify-between text-white pr-2">
@@ -44,7 +44,7 @@
             <div class="h-6">
             </div> 
             <div class="flex justify-between items-end">
-              <span class="bg-green-600 text-white px-4 py-2 rounded-full text-md font-bold">{{ vehicles.price }}</span> 
+              <span class="bg-green-600 text-white px-4 py-2 rounded-full text-md font-bold">{{ vehicle.price }} Rupiah</span> 
               <a class="hover:bg-gray-100 hover:text-gray-900 text-white px-4 py-2 rounded-full font-bold" :href="`/vehicles/${vehicle.id}`">🔐 Top Secret →&#xFEFF;</a>
             </div>
           </div>
@@ -63,7 +63,7 @@ export default {
         }
     },
     mounted(){
-        fetch('http://localhost:3000/vehicles')
+        fetch('https://ipz-jsonserver-dev.vercel.app/vehicles')
         .then(res => res.json())
         .then(data => this.vehicles = data)
         .catch(err => console.log (err.message))
